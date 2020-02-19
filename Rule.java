38c2501ecea75c39c63f11e612a5a25643867080
zachmCS <60157866+zachmCS@users.noreply.github.com>
2020-02-19 02:19:01 -0600

    Added ruleSetter method
    Added rulerSetter method

--- Rule.java
+++ Rule.java
@@ -1,58 +1,32 @@
+import java.io.FileNotFoundException;
+
 @SuppressWarnings("unused")
 public class Rule {
 
-	private String binaryS;
-	private String[] set = { "111", "110", "101", "100", "011", "010", "001", "000" };
-	// boolean array that converts the binary string to false/true
-	// TODO: use to compare to set array
-	private boolean[] setSymbol = new boolean[8];
 	
 
-	public Rule(int ruleNumber) {
-
-		binaryS = Integer.toBinaryString(ruleNumber);
-		int missingLength = 8 - binaryS.length(), i = 0;
-		// Add leading zeros
-		for (i = 0; i < missingLength; i++) {
-			binaryS = 0 + binaryS;
-		}
-
-		for (i = 0; i < 8; i++) {
-			if (binaryS.charAt(i) == '0') {
-				setSymbol[i] = false;
-			} else {
-				setSymbol[i] = true;
-			}
-		}
-	}
-	public String binaryStr (int ruleNum) {
-		binaryS = Integer.toBinaryString(ruleNum);
-		int missingLength = 8 - binaryS.length(), i = 0;
-		// Add leading zeros
-		for (i = 0; i < missingLength; i++) {
-			binaryS = 0 + binaryS;
-		}
-
-		for (i = 0; i < 8; i++) {
-			if (binaryS.charAt(i) == '0') {
-				setSymbol[i] = false;
-			} else {
-				setSymbol[i] = true;
-			}
-		}
-		return binaryS;
-	}
-	
-	public boolean[] getSymbols() {
-
-		return setSymbol;
-
-	}
-	
-	public String[] getSet() {
-		
-		return set;
+	public Rule() {
 		
 	}
 
+	public void ruleSetter(int ruleNum, char[] ruleSet, char falseSymbol, char trueSymbol) {
+		
+		String binaryS = Integer.toBinaryString(ruleNum);
+		int missingLength = 8 - binaryS.length(), i;
+		// Add leading zeros
+		for(i = 0; i < missingLength; i++) {
+			binaryS = 0 + binaryS;
+		}
+		for (int j = ruleSet.length - 1; j >= 0; j--) {
+            ruleSet[j] = falseSymbol;
+        }
+        for (i = ruleSet.length - 1; i >= 0; i--){
+            if (binaryS.charAt(i) == '1'){
+                ruleSet[i] = '1';
+            }
+            else{
+                ruleSet[i] = '0';
+            }
+        }
+	}
 }
