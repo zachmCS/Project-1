561b557a15b5badf661bd1266825007ea3aea1dd
Zachary Muller <60157866+zachmCS@users.noreply.github.com>
2020-02-19 23:33:15 -0600

    Added javadoc

--- Rule.java
+++ Rule.java
@@ -1,32 +1,90 @@
-import java.io.FileNotFoundException;
-
-@SuppressWarnings("unused")
-public class Rule {
-
+import java.util.ArrayList;
+/**
+ * 
+ * @author zachL
+ * The class Generation contains a method that evolves the ECA by setting left, middle,
+ * and right cells based off where they are in a boolean array then it generates based off
+ * a rule set. 
+ */
+public class Generation{
 	
-
-	public Rule() {
+	public Generation() {
 		
 	}
 
-	public void ruleSetter(int ruleNum, char[] ruleSet, char falseSymbol, char trueSymbol) {
-		
-		String binaryS = Integer.toBinaryString(ruleNum);
-		int missingLength = 8 - binaryS.length(), i;
-		// Add leading zeros
-		for(i = 0; i < missingLength; i++) {
-			binaryS = 0 + binaryS;
+	Rule rule = new Rule();
+	
+	/**
+	 * This method is the generator for the ECA.
+	 * @param ECA
+	 * @param ruleNum
+	 * @param initState
+	 * @param currentGen
+	 * @param ruleSet
+	 * @param falseSymbol
+	 * @param trueSymbol
+	 */
+	public void generate(ArrayList<boolean[]> ECA, int ruleNum, boolean[] initState, ArrayList<Character> currentGen, char[] ruleSet, char falseSymbol, char trueSymbol) {
+		int leftCell, middleCell, rightCell;
+		rule.ruleSetter(ruleNum, ruleSet, falseSymbol, trueSymbol);
+		for (int i = 0; i < initState.length; i++) {
+			if (i == 0) {
+				leftCell = initState.length - 1;
+				middleCell = i;
+				rightCell = i + 1;
+
+			} else if (i == initState.length - 1) {
+				leftCell = i - 1;
+				middleCell = i;
+				rightCell = 0;
+
+			} else {
+				leftCell = i - 1;
+				middleCell = i;
+				rightCell = i + 1;
+			}
+
+			if (initState[middleCell] == true && initState[leftCell] == true && initState[rightCell] == true) {
+				currentGen.add(i, (ruleSet[0]));
+			} else if (initState[middleCell] == true && initState[leftCell] == true && initState[rightCell] == false) {
+				currentGen.add(i, (ruleSet[1]));
+			} else if (initState[middleCell] == false && initState[leftCell] == true && initState[rightCell] == true) {
+				currentGen.add(i, (ruleSet[2]));
+			} else if (initState[middleCell] == false && initState[leftCell] == true && initState[rightCell] == false) {
+				currentGen.add(i, (ruleSet[3]));
+			} else if (initState[middleCell] == true && initState[leftCell] == false && initState[rightCell] == true) {
+				currentGen.add(i, (ruleSet[4]));
+			} else if (initState[middleCell] == true && initState[leftCell] == false && initState[rightCell] == false) {
+				currentGen.add(i, (ruleSet[5]));
+			} else if (initState[middleCell] == false && initState[leftCell] == false && initState[rightCell] == true) {
+				currentGen.add(i, (ruleSet[6]));
+			} else if (initState[middleCell] == false && initState[leftCell] == false && initState[rightCell] == false) {
+				currentGen.add(i, (ruleSet[7]));
+			}
 		}
-		for (int j = ruleSet.length - 1; j >= 0; j--) {
-            ruleSet[j] = falseSymbol;
-        }
-        for (i = ruleSet.length - 1; i >= 0; i--){
-            if (binaryS.charAt(i) == '1'){
-                ruleSet[i] = '1';
-            }
-            else{
-                ruleSet[i] = '0';
-            }
-        }
+		initState = binToBool(currentGen);
+		ECA.add(initState);
+		currentGen.clear();
 	}
+	/**
+	 * This method converts binary elements from currentGen to false and true with respect to 
+	 * 0 and 1. 
+	 * @param currentGen
+	 * @return
+	 */
+	public boolean[] binToBool(ArrayList<Character> currentGen) {
+		boolean[] temp = new boolean[currentGen.size()];
+		for (int i = 0; i < currentGen.size(); i++) {
+			char currentState = currentGen.get(i);
+			if (currentState == '0') {
+				temp[i] = false;
+			} else {
+				temp[i] = true;
+			}
+		}
+		return temp;
+	}
+	
+	
+	
 }
